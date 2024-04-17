1d6a9d49c83e7b7fb08816a670016fe5baa7f025
KentaToya <163114083+kenta1129@users.noreply.github.com>
2024-04-17 12:36:11 +0000

    [Finish]10-22

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,7 +1,17 @@
 class PostImage < ApplicationRecord
 
-has_one_attached :image
 belongs_to :user
+has_one_attached :image
+has_many :post_comments, dependent: :destroy
+has_many :favorites, dependent: :destroy
+
+
+validates :shop_name, presence: true
+ validates :image, presence: true
+
+
+
+
 
 def get_image
     unless image.attached?
@@ -11,6 +21,8 @@ def get_image
     image
 end
 
-
+def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,20 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
   
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  resource :favorite, only: [:create, :destroy]
+  resources :post_comments, only: [:create, :destroy, :index]
+  
+end
+  
+  resources :users, only: [:show, :edit, :update]
 
-  get '/homes/about', to: 'homes#about', as: 'about'
+  
   
   devise_scope :user do
   get '/users/sign_out', to: 'devise/sessions#destroy'
 end
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
